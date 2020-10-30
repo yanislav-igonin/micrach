5f86a533570414517ac56023f971d42649691836
Yanislav Igonin <yanislavdabest@gmail.com>
2020-10-30 22:46:27 +0300

    feat: add env config

--- src/config/app.ts
+++ src/config/app.ts
@@ -1,5 +1,6 @@
 export const app = {
+  env: process.env.NODE_ENV || 'production',
   port: process.env.PORT !== '' && process.env.PORT !== undefined
     ? parseInt(process.env.PORT, 10)
-    : 3000,
+    : 80,
 };
