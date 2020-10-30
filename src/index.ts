f07020113c35ca78327723e46fe7cd42380714f1
Yanislav Igonin <yanislavdabest@gmail.com>
2020-10-30 20:45:17 +0300

    feat: add basic server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log('kek')
+import Koa from 'koa';
+
+import { config } from './config';
+
+const app = new Koa();
+
+app.listen(config.app.port);
+console.log('app listenning on port', config.app.port);
+console.log('all systems nominal');
