63dd2dedeabdcc65bc7265a54f68ffc6d9461873
Yanislav Igonin <yanislavdabest@gmail.com>
2020-10-31 19:41:49 +0300

    feat: add threads ordering

--- src/modules/threads/repositories/threads.repository.ts
+++ src/modules/threads/repositories/threads.repository.ts
@@ -4,7 +4,7 @@ import { Thread } from '../entities/thread.entity';
 @EntityRepository(Thread)
 export class ThreadsRepository extends Repository<Thread> {
   getAll() {
-    return this.find({ relations: ['posts'] });
+    return this.find({ relations: ['posts'], order: { updatedAt: 'DESC' } });
   }
 
   createOne() {
