97f6df4e895fbc28cfb7f1b23460fc68dbca0044
Yanislav Igonin <yanislavdabest@gmail.com>
2020-11-02 03:24:38 +0300

    feat: update load test

--- tests/load/autocannon.sh
+++ tests/load/autocannon.sh
@@ -1,8 +1,21 @@
+# Create Thread
+# POST /threads
+autocannon -c 1000 -d 60 -r 1000 -m 'POST' -b 'post.json' -H 'Content-Type: application/json' localhost:3000/threads
+
+sleep 10
+
+# Create Post
+# POST /threads/:id
+autocannon -c 1000 -d 60 -r 1000 -m 'POST' -b 'post.json' -H 'Content-Type: application/json' localhost:3000/threads/1
+
+sleep 10
+
+# Get Threads
 # GET /threads
 autocannon -c 1000 -d 60 -r 1000 localhost:3000/threads
 
-# GET /threads/:id
-autocannon -c 1000 -d 60 -r 1000 localhost:3000/threads/19
+sleep 10
 
-# POST /threads
-autocannon -c 1000 -d 60 -r 1000 -m 'POST' -b 'thread.json' -H 'Content-Type: application/json' localhost:3000/threads
+# Get Thread
+# GET /threads/:id
+autocannon -c 1000 -d 60 -r 1000 localhost:3000/threads/1
