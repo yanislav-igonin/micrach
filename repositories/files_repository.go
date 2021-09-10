545a13180112ef9d7ea202bda55d5b18c48c3bba
Yanislav Igonin <yanislavdabest@gmail.com>
2021-09-10 18:18:52 +0300

    feat: remove conn aquire in file create

--- repositories/files_repository.go
+++ repositories/files_repository.go
@@ -13,22 +13,16 @@ type FilesRepository struct{}
 var Files FilesRepository
 
 func (r *FilesRepository) Create(f File) error {
-	conn, err := Db.Pool.Acquire(context.TODO())
-	if err != nil {
-		return err
-	}
-	defer conn.Release()
-
 	sql := `
 		INSERT INTO files (post_id, name, ext, size)
 		VALUES ($1, $2, $3, $4)
 	`
 
-	row := conn.QueryRow(
+	row := Db.Pool.QueryRow(
 		context.TODO(), sql, f.PostID, f.Name, f.Ext, f.Size,
 	)
 
-	err = row.Scan()
+	err := row.Scan()
 	if err != nil && err != pgx.ErrNoRows {
 		return err
 	}
