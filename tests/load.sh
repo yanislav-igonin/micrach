2ee326d5f46f7f6c431f167034873bd2abc0a48b
Yanislav Igonin <yanislavdabest@gmail.com>
2022-05-21 13:29:10 +0300

    lint

--- tests/load.sh
+++ tests/load.sh
@@ -1,5 +1,5 @@
-  echo "GET http://micrach.igonin.dev" | vegeta attack -rate 100 -duration=120s | tee results.bin | vegeta report
-  # vegeta report -type=json results.bin > metrics.json
-  cat results.bin | vegeta plot > plot.html
-  open plot.html
-  # cat results.bin | vegeta report -type="hist[0,100ms,200ms,300ms]"
+echo "GET http://micrach.igonin.dev" | vegeta attack -rate 60 -duration=120s | tee results.bin | vegeta report
+# vegeta report -type=json results.bin > metrics.json
+cat results.bin | vegeta plot > plot.html
+open plot.html
+# cat results.bin | vegeta report -type="hist[0,100ms,200ms,300ms]"
